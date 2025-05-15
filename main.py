e308c93480cf9d57507400a421ea8da13aaa2959
vinicius-souzaa <vini.v4@hotmail.com>
2025-05-15 15:49:07 -0300

    main.py

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ from util.layout import layout_saida
 warnings.filterwarnings("ignore")
 #locale.setlocale(locale.LC_ALL, "pt_BR.UTF-8")
 
-st.set_page_config(page_title='Tech Challenge 4', layout="wide")
+st.set_page_config(page_title='Tech Challenge 4', layout="wide",page_icon="🛠️")
 
 layout_saida()
 
